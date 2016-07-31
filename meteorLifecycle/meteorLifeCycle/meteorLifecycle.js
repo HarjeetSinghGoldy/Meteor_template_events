7b8f555e9b312d05d1e816a25f804272f425036a
harjeet <harjeetsingh@cybuzzsc.com>
2016-07-31 18:20:37 +0530

    Changes

--- meteorLifecycle/meteorLifeCycle/meteorLifecycle.js
+++ meteorLifecycle/meteorLifeCycle/meteorLifecycle.js
@@ -1,12 +1,15 @@
 if (Meteor.isClient) {
+  //////Execute at the first time when template is created(Instance is there but template is not visible)
   Template.profile.onCreated(function () {
     this.lastCallback = 'created';
     console.log('profile.created', this);
   });
+  ///////// after on created instace and table is visible
   Template.profile.onRendered(function () {
     this.lastCallback = 'rendered';
     console.log('profile.rendered', this);
   });
+  /////////// both instance and template visiblty is not there
   Template.profile.onDestroyed(function () {
     this.lastCallback = 'destroyed';
     console.log('profile.destroyed', this);
